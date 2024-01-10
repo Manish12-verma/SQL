8389353b10c73c079026370dc79b6b8554f8641b
Manish verma <110876979+Manish12-verma@users.noreply.github.com>
2024-01-11 00:46:44 +0530

    Add files via upload

---
+++ first.sql
@@ -0,0 +1,24 @@
+create database college;
+use college;
+
+create table student(
+rollno int primary key,
+name varchar(50),
+marks int not null,
+grade varchar(1),
+city varchar(20)
+);
+
+insert into student values
+(101,"Anil",78,"C","Pune"),
+(102,"Ram",787,"A","Delhi"),
+(103,"Hari",365,"B","Mumbai"),
+(104,"Shyam",870,"D","Delhi"),
+(105,"Jari",305,"B","Mumbai");
+
+select city ,count(rollno)
+from student
+group by city
+having max(marks)>400;
+
+
